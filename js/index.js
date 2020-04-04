655614a44917be9aaf2d02b44b7c1ce0ef65e2ff
psdxll <51881917+psdxll@users.noreply.github.com>
2020-04-05 02:00:13 +0800

    Delete index.js

--- js/index.js
+++
@@ -1,8 +0,0 @@
-var video = document.getElementById('video1');
-video.playbackRate = 0.5;
-//视频变速
-
-window.addEventListener("scroll", function () {
-	var header = document.querySelector("header");
-	header.classList.toggle("sticky", window.scrollY > 0);
-  })
